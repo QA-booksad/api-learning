026397adff59d86ffa29aba8f7d2f4011316d346
Maria <zyryansk@gmail.com>
2026-05-17 16:10:07 +0000

    add allure reporting

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,20 +1,37 @@
 import requests
+import allure
 
 BASE_URL = "http://127.0.0.1:8000"
 
 
+@allure.feature("Root")
+@allure.story("Check root endpoint")
 def test_root():
-    response = requests.get(BASE_URL)
 
-    assert response.status_code == 200
-    assert response.json()["message"] == "Hello QAaa"
+    with allure.step("Send GET request to root endpoint"):
+        response = requests.get(BASE_URL)
+
+    with allure.step("Check status code"):
+        assert response.status_code == 200
+
+    with allure.step("Check response body"):
+        assert response.json()["message"] == "Hello QA"
 
 
+@allure.feature("Users")
+@allure.story("Get user by id")
 def test_get_user():
-    response = requests.get(f"{BASE_URL}/users/1")
+
+    with allure.step("Send GET request to /users/1"):
+        response = requests.get(f"{BASE_URL}/users/1")
 
     body = response.json()
 
-    assert response.status_code == 200
-    assert body["id"] == 1
-    assert body["name"] == "Maria"
+    with allure.step("Check status code"):
+        assert response.status_code == 200
+
+    with allure.step("Check user id"):
+        assert body["id"] == 1
+
+    with allure.step("Check user name"):
+        assert body["name"] == "Vasya"
